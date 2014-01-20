620b50b40fc4716756cd9d773965baf4ddda2f06
Greg Kettler <gkettler@alum.mit.edu>
2014-01-19 19:16:47 -0600

    based on tutorial

--- xmonad.hs
+++ xmonad.hs
@@ -1,7 +1,24 @@
 import XMonad
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.DynamicLog
+import XMonad.Util.Run(spawnPipe)
+import System.IO
 
-main = xmonad $ defaultConfig
-    { borderWidth = 2
-    ,
-    terminal = "gnome-terminal"
+main = do
+    xmproc <- spawnPipe "/usr/bin/xmobar"
+    xmonad $ defaultConfig {
+        borderWidth = 2
+        ,
+        terminal = "urxvt"
+        ,
+        modMask = mod4Mask     -- Rebind Mod to the Windows key
+        ,
+        manageHook = manageDocks <+> manageHook defaultConfig
+        ,
+        logHook = dynamicLogWithPP xmobarPP {
+            ppOutput = hPutStrLn xmproc
+            , ppTitle = xmobarColor "green" "" . shorten 50
+        }
+
+
     }
